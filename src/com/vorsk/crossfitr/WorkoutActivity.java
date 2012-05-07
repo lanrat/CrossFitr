1aed11e68b81f7d28e028dd4f3faca2e8c0fd508
Han Yoo <me_thm@hotmail.com>
2012-05-06 17:29:30 -0700

    d

---
+++ src/com/vorsk/crossfitr/WorkoutActivity.java
@@ -0,0 +1,56 @@
+package com.vorsk.crossfitr;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class WorkoutActivity extends Activity implements OnClickListener{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.workouts);
+        
+        View wodButton = findViewById(R.id.wod_button);
+        wodButton.setOnClickListener(this);
+        View allButton = findViewById(R.id.all_button);
+        allButton.setOnClickListener(this);
+        View customButton = findViewById(R.id.custom_button);
+        customButton.setOnClickListener(this);
+        View heroButton = findViewById(R.id.hero_button);
+        heroButton.setOnClickListener(this);
+        View girlButton = findViewById(R.id.girl_button);
+        girlButton.setOnClickListener(this);        
+    }
+    
+    public void onClick(View v)
+    {
+    	switch(v.getId())
+    	{
+    		case R.id.all_button:
+    			Intent i = new Intent(this, AllActivity.class);
+    			startActivity(i);
+    			break;
+    			
+    		case R.id.wod_button:
+    			i = new Intent(this, WodActivity.class);
+    			startActivity(i);
+    			break;
+    			
+    		case R.id.girl_button:
+    			i = new Intent(this, GirlsActivity.class);
+    			startActivity(i);
+    			break;
+    		case R.id.hero_button:
+    			i = new Intent(this, HerosActivity.class);
+    			startActivity(i);
+    			break;
+    		case R.id.custom_button:
+    			i = new Intent(this, CustomActivity.class);
+    			startActivity(i);
+    			break;
+    	}
+    }
+}
