a2f3465abb8b2a500c4d329b4f8b8dc2a84924c6
Darren Seung Won <Darren Seung Won@128.54.50.234>
2012-05-13 16:00:49 -0700

    committing

---
+++ src/com/vorsk/crossfitr/GirlsActivity.java
@@ -0,0 +1,27 @@
+package com.vorsk.crossfitr;
+
+import android.app.Activity;
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+
+
+public class GirlsActivity extends ListActivity
+{
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		
+		String[] GIRLS = new String[] { "GirlA", "GirlB", "GirlC", "GirlD", "GirlE" };
+		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, android.R.id.text1, GIRLS);
+		
+		setListAdapter(adapter);
+	}
+}
---
+++ src/com/vorsk/crossfitr/UserProfileActivity.java
@@ -0,0 +1,32 @@
+package com.vorsk.crossfitr;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class UserProfileActivity extends Activity implements OnClickListener
+{
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.userprofile);
+		
+		View user_profile_button = findViewById(R.id.edit_profile_button);
+	    user_profile_button.setOnClickListener(this);
+	}
+
+	
+	public void onClick(View v) 
+	{
+		// TODO Auto-generated method stub
+		switch (v.getId()) 
+		{
+		case R.id.edit_profile_button:
+			Intent u = new Intent(this, EditUserProfileActivity.class);
+			startActivity(u);
+			break;
+		}
+	}
+}
