2f641aa282d6ccc0bd454c68e70edf5b46172073
Han Yoo <me_thm@hotmail.com>
2012-05-13 16:51:49 -0700

    added toast to heroactivity.java

--- src/com/vorsk/crossfitr/HeroesActivity.java
+++ src/com/vorsk/crossfitr/HeroesActivity.java
@@ -1,12 +1,17 @@
 package com.vorsk.crossfitr;
 
 import android.app.Activity;
+
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class HeroesActivity extends ListActivity
 {
@@ -14,13 +19,16 @@ public class HeroesActivity extends ListActivity
 	{
 		super.onCreate(savedInstanceState);
 		
-		String[] HEROES = new String[] { "captain", "hulk", "hero" }; 
+		final String[] HEROES = new String[] { "captain", "hulk", "hero" }; 
 		
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
 				android.R.layout.simple_list_item_1, android.R.id.text1, HEROES);
 		
 		setListAdapter(adapter);
-		
-		
 	}
+		public void onListItemClick(ListView list, View view, int position, long id)
+		{
+			String item = (String) getListAdapter().getItem(position);
+			Toast.makeText(this, item + " selected", Toast.LENGTH_SHORT).show();
+		}	
 }
