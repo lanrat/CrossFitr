6789ec5702eb832488d1be36363dca40e7753c1d
Han Yoo <me_thm@hotmail.com>
2012-05-13 16:04:22 -0700

    added list view for HeroesActivity.java

--- src/com/vorsk/crossfitr/HeroesActivity.java
+++ src/com/vorsk/crossfitr/HeroesActivity.java
@@ -1,18 +1,25 @@
 package com.vorsk.crossfitr;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
 
-public class HeroesActivity extends Activity
+public class HeroesActivity extends ListActivity
 {
-	static final String[] heroes = { "captain", "hulk", "ironmask" }; 
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.workout_list);
+		
+		String[] HEROES = new String[] { "captain", "hulk", "hero" }; 
+		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, android.R.id.text1, HEROES);
+		
+		setListAdapter(adapter);
 		
 		
 	}
