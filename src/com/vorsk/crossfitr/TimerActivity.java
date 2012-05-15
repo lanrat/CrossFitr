7e81ec1ba36545b399af8dd7a20b7e9cba8dd862
Matthew <mkoonce@ucsd.edu>
2012-05-15 15:20:56 -0700

    Added rudimentary implementation of a timer popup

--- src/com/vorsk/crossfitr/TimerActivity.java
+++ src/com/vorsk/crossfitr/TimerActivity.java
@@ -1,19 +1,74 @@
 package com.vorsk.crossfitr;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.TextView;
-
+import android.widget.TimePicker;
+/*
+ * This is contains a very rudimentary implementation of the timer popup. 
+ * Jason: Feel free to change any of this. All variables are just there to make it work.
+ */
 public class TimerActivity extends Activity 
 {
+	 private Button mPickTime;
+	 private TextView mTimerDisplay;
+
+	 private int mSec = 0;
+	 private int mMinute = 0;
+
+	 static final int TIME_DIALOG_ID = 0; // Dialog variable
+	    
 	public void onCreate(Bundle savedInstanceState) 
 	{
 		super.onCreate(savedInstanceState);
-		TextView textview = new TextView(this);
-		textview.setText("This is the Timer tab");
-		setContentView(textview);
+		setContentView(R.layout.timer_tab);
+		
+		mPickTime = (Button) findViewById(R.id.pickTime);
+		mTimerDisplay = (TextView) findViewById(R.id.timer_label);
+		
+		// Opens Dialog on click
+		mPickTime.setOnClickListener(new View.OnClickListener()
+		{
+			public void onClick(View v) 
+			{
+	            showDialog(TIME_DIALOG_ID);
+	        }
+	    });
+	}
+
+	// Sets the variables after the user selects a time. Updates text field 
+	private TimePickerDialog.OnTimeSetListener mTimeSetListener =
+		    new TimePickerDialog.OnTimeSetListener() {
+		        public void onTimeSet(TimePicker view, int minute, int seconds) {
+		            mSec = seconds;
+		            mMinute = minute;
+		            mTimerDisplay.setText("You chose: " +  new StringBuilder().append(pad(mMinute)).append(".").append(pad(mSec)));
+		        }
+	};
+	
+	// Appends a 0 to the front of a single character 
+	private static String pad(int c) {
+	    if (c >= 10)
+	        return String.valueOf(c);
+	    else
+	        return "0" + String.valueOf(c);
+	}
+	
+	// Creates the Dialog
+	@Override
+	protected Dialog onCreateDialog(int id) 
+	{
+		switch (id) 
+		{
+			case TIME_DIALOG_ID:
+		    return new TimePickerDialog(this, mTimeSetListener, mMinute, mSec, false);
+		 }
+		 return null;
 	}
 }
