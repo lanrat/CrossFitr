a94d0e6d7b63caa037f896cd8b0f0e6b2dc08659
Ian Foster <mrlanrat@gmail.com>
2012-05-20 17:23:04 -0700

    improved rss parser

--- src/com/vorsk/crossfitr/WodActivity.java
+++ src/com/vorsk/crossfitr/WodActivity.java
@@ -1,16 +1,12 @@
 package com.vorsk.crossfitr;
 
 import com.vorsk.crossfitr.models.WODModel;
+import com.vorsk.crossfitr.models.WorkoutRow;
 
 import android.app.Activity;
-import android.app.ListActivity;
 import android.os.Bundle;
-import android.content.Intent;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 public class WodActivity extends Activity 
 {
@@ -20,10 +16,12 @@ public class WodActivity extends Activity
 		setContentView(R.layout.workout_list);
 
 		ListView listView = (ListView) findViewById(R.id.workout_list);
+		
+		WODModel model = new WODModel();
 
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1, android.R.id.text1,
-				new WODModel().list);
+		ArrayAdapter<WorkoutRow> adapter = new ArrayAdapter<WorkoutRow>(this,
+				android.R.layout.simple_list_item_1, android.R.id.text1,model.getWodRows() );
+
 
 		listView.setAdapter(adapter);
 	}
--- src/com/vorsk/crossfitr/models/WODModel.java
+++ src/com/vorsk/crossfitr/models/WODModel.java
@@ -1,50 +1,60 @@
 package com.vorsk.crossfitr.models;
 
-import java.io.InputStream;
-import java.net.URL;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
-import android.sax.Element;
-import android.sax.EndTextElementListener;
-import android.sax.RootElement;
-import android.util.Log;
-import android.util.Xml;
+import org.horrabin.horrorss.*;
 
 public class WODModel {
 	public static final String feed_url = "http://feeds.feedburner.com/crossfit/eRTq?format=xml";
-	private URL url;
 
-	// TODO: rm
-	public ArrayList<String> list = new ArrayList<String>();
+	RssParser rss = new RssParser();
+
+	private String title;
+	private ArrayList<WorkoutRow> list = new ArrayList<WorkoutRow>();
 
 	public WODModel() {
-		RootElement root = new RootElement("rss");
-		Element itemlist = root.getChild("channel");
 		
-		Element item = itemlist.getChild("item");
-
-		item.getChild("title").setEndTextElementListener(
-				new EndTextElementListener() {
-					public void end(String title) {
-						list.add(title.substring(0, title.indexOf(' ')));
-					}
-				});
-
-		item.getChild("description").setEndTextElementListener(
-				new EndTextElementListener() {
-					public void end(String title) {
-						//list.add(title.substring(0, title.indexOf(' ')));
-					}
-				});
-
-		try {
-			url = new URL(feed_url);
-			InputStream input = url.openConnection().getInputStream();
-			Xml.parse(input, Xml.Encoding.UTF_8, root.getContentHandler());
-		} catch (Exception e) {
-			Log.e("RSSParser", e.toString());
+		try{
+			RssFeed feed = rss.load(feed_url);
+			
+			RssChannelBean channel = feed.getChannel();
+			this.title = channel.getTitle();
+			
+			// Gets and iterate the items of the feed 
+			List<RssItemBean> items = feed.getItems();
+			Iterator<RssItemBean> it = items.iterator();
+			
+			RssItemBean item;
+			WorkoutRow row;
+			while (it.hasNext()){
+				item = it.next();
+				row = new WorkoutRow();
+				
+				row.name = item.getTitle();
+				row.description = item.getDescription();
+				row.workout_type_id = SQLiteDAO.TYPE_WOD;
+				row.record = WorkoutModel.NOT_SCORED;
+				row.record_type_id = WorkoutModel.SCORE_NONE; 
+				list.add(row);
+			}
+			
+		}catch(Exception e){
+			//TODO  Something to do if an exception occurs
+			WorkoutRow row = new WorkoutRow();
+			row.name = "Error Loading RSS";
+			list.add(row);
 		}
 
+	}
+	
+	public String getTitle(){
+		return this.title;
+	}
+	
+	public ArrayList<WorkoutRow> getWodRows(){
+		return this.list;
 	}
 
 }
