952b25291feb6b7f9fb02b77f362e1469e0e48e1
Matthew <mkoonce@ucsd.edu>
2012-05-28 19:03:05 -0700

    Created working version of the timer. Still some bugs to fix. Edited associated xmls. Renamed stopwatch.xml to stopwatch_tab.xml for consistancy.

--- src/com/vorsk/crossfitr/TimerActivity.java
+++ src/com/vorsk/crossfitr/TimerActivity.java
@@ -7,6 +7,7 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -15,12 +16,13 @@ import android.content.Context;
 
 public class TimerActivity extends Activity 
 {	
+	private static String TAG = "TimerActivity";
     static final int NUMBER_DIALOG_ID = 0; // Dialog variable
     TextView mTimeDisplay, mElapsedTime;
     private int mHour, mMin, mSec;
     NumberPicker mNumberPicker;
-    Button mStart, mPause, mReset;
-    Time mTime = new Time();
+    Button mStart, mStop, mReset, mSetTimer;
+    Time timer = new Time();
     
     
 	// Timer to update the elapsedTime display
@@ -28,7 +30,7 @@ public class TimerActivity extends Activity
     private final int TICK_WHAT = 2; 
 	private Handler mHandler = new Handler() {
         public void handleMessage(Message m) {
-        	//updateElapsedTime();
+        	updateElapsedTime();
         	sendMessageDelayed(Message.obtain(this, TICK_WHAT), mFrequency);
         }
     };
@@ -37,20 +39,17 @@ public class TimerActivity extends Activity
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.timer_tab);
-
-		mTimeDisplay = (TextView) findViewById(R.id.timer_label);
-		Button mSelectTimer = (Button) findViewById(R.id.pickTime);
 		
 		mElapsedTime = (TextView)findViewById(R.id.ElapsedTime);
         
         mStart = (Button)findViewById(R.id.StartButton);
-        mPause = (Button)findViewById(R.id.PauseButton);
-        mReset = (Button)findViewById(R.id.ResetButton);
+        mStop = (Button)findViewById(R.id.StopButton);
+        mSetTimer = (Button)findViewById(R.id.SetTimer);
         
         mHandler.sendMessageDelayed(Message.obtain(mHandler, TICK_WHAT), mFrequency);
     
 		// Opens Dialog on click
-		mSelectTimer.setOnClickListener(new View.OnClickListener()
+		mSetTimer.setOnClickListener(new View.OnClickListener()
 		{
 			public void onClick(View v) 
 			{
@@ -65,74 +64,102 @@ public class TimerActivity extends Activity
 					mHour = selectedHour;
 					mMin = selectedMin;
 					mSec = selectedSec;
-					updateDisplay();
+					mElapsedTime.setText(new StringBuilder().append(mHour)
+					    	.append(":").append(formatDigits(mMin)).append(":").append(formatDigits(mSec)));
+					showStartButton();
 				}
 
 		    };
 
-	private void updateDisplay() {
-		if(mHour == 0){
-			MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.countdown_00);
-			mediaPlayer.start();
-		}
-		mTimeDisplay.setText(new StringBuilder().append(mHour)
-	    	.append(":").append(pad(mMin)).append(":").append(pad(mSec)));
-	}
 	
-	public void updateElapsedTime() {
-		//mElapsedTime.setText(formatElapsedTime(mTime.getElapsedTime()));
+	private void updateElapsedTime() {
+		mElapsedTime.setText(getFormattedElapsedTime());
 	}
 	 
-	private String formatElapsedTime(long now) {
-		long hours=0, minutes=0, seconds=0, tenths=0;
-		StringBuilder sb = new StringBuilder();
+	public long getStartTime(){
+		long startTime = (mHour * 3600000) + (mMin * 60000) + (mSec * 1000);
+ 	    return startTime;
+    }
 
-		if (now < 1000) {
-			tenths = now / 100;
-		} else if (now < 60000) {
-			seconds = now / 1000;
-			now -= seconds * 1000;
-			tenths = now / 100;
-		} else if (now < 3600000) {
-			hours = now / 3600000;
-			now -= hours * 3600000;
-			minutes = now / 60000;
-			now -= minutes * 60000;
-			seconds = now / 1000;
-			now -= seconds * 1000;
-			tenths = now / 100;
+	private String formatElapsedTime(long start) {
+		long hours = 0;
+		long minutes = 0;
+		long seconds = 0;
+		long tenths = 0;
+		StringBuilder sb = new StringBuilder();
+		if(start <= 0){
+			timer.stop();
 		}
 		
-		if (hours > 0) {
-			sb.append(hours).append(":")
-				.append(formatDigits(minutes)).append(":")
-				.append(formatDigits(seconds)).append(".")
-				.append(tenths);
-		} else {
-			sb.append(formatDigits(minutes)).append(":")
+		if (start < 1000) {
+			tenths = start / 100;
+		} 
+		
+		else if (start < 60000) 
+		{
+			seconds = start / 1000;
+			start -= seconds * 1000;
+			tenths = start / 100;
+		}
+		
+		else if (start < 3600000) 
+		{
+			hours = start / 3600000;
+			start -= hours * 3600000;
+			minutes = start / 60000;
+			start -= minutes * 60000;
+			seconds = start / 1000;
+			start -= seconds * 1000;
+			tenths = start / 100;
+		}
+			
+		sb.append(hours).append(":")
+			.append(formatDigits(minutes)).append(":")
 			.append(formatDigits(seconds)).append(".")
 			.append(tenths);
-		}
-		
+	
 		return sb.toString();
 	}
+	
+	public String getFormattedElapsedTime() {
+		return formatElapsedTime(getStartTime() - (getElapsedTime() % 30000));
+	}
+	
+	public long getElapsedTime() {
+		return timer.getElapsedTime();
+	}
+	
+	public void onStartClicked(View V) {
+		Log.d(TAG, "start button clicked");
+		timer.start();
+		showPauseButton();
+	}
+
+	public void onStopClicked(View v) {
+		Log.d(TAG, "pause button clicked");
+		timer.stop();
+		showStartButton();
+	}
+	
+	private void showPauseButton() {
+		Log.d(TAG, "showPauseLapButtons");
+
+		mStart.setVisibility(View.GONE);
+		mStop.setVisibility(View.VISIBLE);
+	}
+
+	private void showStartButton() {
+		Log.d(TAG, "showStartResetButtons");
+
+		mStart.setVisibility(View.VISIBLE);
+		mStop.setVisibility(View.GONE);
+	}
+
 	
 	private String formatDigits(long num) {
 		return (num < 10) ? "0" + num : new Long(num).toString();
 	}
 	
-	
-	/**
-	 * Adds '0' in front of values less than 10
-	 * @param c
-	 * @return value with '0' 
-	 */
-	private static String pad(int c) {
-	    if (c >= 10)
-	        return String.valueOf(c);
-	    else
-	        return "0" + String.valueOf(c);
-	}
 	
 	// Override creating the Dialog
 	@Override
