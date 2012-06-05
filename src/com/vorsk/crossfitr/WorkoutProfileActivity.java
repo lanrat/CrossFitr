8865499ca513100b1629cce74a557dbacf86de3a
Matthew <mkoonce@ucsd.edu>
2012-06-05 16:21:11 -0700

    Instead of a text field, there is now a popup if the workout is score_type reps or weight so the user can enter this info.

--- src/com/vorsk/crossfitr/WorkoutProfileActivity.java
+++ src/com/vorsk/crossfitr/WorkoutProfileActivity.java
@@ -5,8 +5,11 @@ import com.vorsk.crossfitr.models.WorkoutRow;
 import com.vorsk.crossfitr.models.WorkoutSessionModel;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -19,7 +22,6 @@ public class WorkoutProfileActivity extends Activity implements OnClickListener
 	private WorkoutRow workout;
 	private int ACT_TIMER = 1;
 	private TextView tvextra;
-	private EditText etextra;
 	
 	//Its dynamic! android should use this by default
 	private String TAG = this.getClass().getName();
@@ -49,23 +51,12 @@ public class WorkoutProfileActivity extends Activity implements OnClickListener
 		TextView tvdesc = (TextView)findViewById(R.id.workout_profile_descDB);
 		//TextView tvrecordType = (TextView)findViewById(R.id.workout_profile_recordtypeDB);
 		tvextra = (TextView)findViewById(R.id.label_extra_info);
-		etextra = (EditText)findViewById(R.id.text_extra_info);
 		
 		//set the texts of the TextView objects from the data retrieved from the DB
 		tvname.setText(workout.name);
 		tvdesc.setText(workout.description);
 		//tvrecordType.setText(model.getTypeName(workout.workout_type_id));
 		tvbestRecord.setText(StopwatchActivity.formatElapsedTime(Long.parseLong(String.valueOf(workout.record))));
-		
-		if (workout.record_type_id == WorkoutModel.SCORE_NONE
-				|| workout.record_type_id == WorkoutModel.SCORE_TIME) {
-			tvextra.setVisibility(View.GONE);
-			etextra.setVisibility(View.GONE);
-		} else if (workout.record_type_id == WorkoutModel.SCORE_WEIGHT) {
-			tvextra.setText("Weight:");
-		} else {
-			tvextra.setText("Reps:");
-		}
 		
 		// begin workout button
         View beginButton = findViewById(R.id.button_begin_workout);
@@ -79,15 +70,89 @@ public class WorkoutProfileActivity extends Activity implements OnClickListener
 		{
 		    // if user presses begin button, user will now go into the timer page.
 			case R.id.button_begin_workout:
-				Intent i = new Intent(this, TimeTabWidget.class);
-				i.putExtra("workout_id", workout._id);
-				i.putExtra("workout_score",
-						Integer.parseInt(etextra.getText().toString()));
-				startActivityForResult(i, ACT_TIMER);
+				if(workout.record_type_id == WorkoutModel.SCORE_WEIGHT){
+					weightPopup();
+				}
+				
+				else if(workout.record_type_id == WorkoutModel.SCORE_REPS){
+					repsPopup();
+				}
+				
+				else{	
+					Intent i = new Intent(this, TimeTabWidget.class);
+					i.putExtra("workout_id", workout._id);
+					//i.putExtra("workout_score",
+							//Integer.parseInt(etextra.getText().toString()));
+					startActivityForResult(i, ACT_TIMER);
+				} 
 				break;
 		}
 	}
 	
+	
+	
+	private void repsPopup() {
+		// TODO Auto-generated method stub
+		final Intent i = new Intent(this, TimeTabWidget.class);
+		AlertDialog.Builder alert = new AlertDialog.Builder(this);
+		
+		alert.setMessage("Enter the number of reps:");
+
+		// Set an EditText view to get user input 
+		final EditText input = new EditText(this);
+		alert.setView(input);
+
+		alert.setPositiveButton("Begin", new DialogInterface.OnClickListener() {
+		public void onClick(DialogInterface dialog, int whichButton) {
+		  Editable value = input.getText();
+			i.putExtra("workout_id", workout._id);
+			i.putExtra("workout_score",
+					Integer.parseInt(value.toString()));
+			startActivityForResult(i, ACT_TIMER);
+		  }
+		});
+
+		alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+		  public void onClick(DialogInterface dialog, int whichButton) {
+		    // Canceled. Do nothing
+		  }
+		});
+
+		alert.show();
+	}
+
+	private void weightPopup() {
+		// TODO Auto-generated method stub
+		AlertDialog.Builder alert = new AlertDialog.Builder(this);
+		final Intent i = new Intent(this, TimeTabWidget.class);
+		
+		alert.setMessage("Enter the weight you'll use:");
+
+		// Set an EditText view to get user input 
+		final EditText input = new EditText(this);
+		alert.setView(input);
+
+		alert.setPositiveButton("Begin", new DialogInterface.OnClickListener() {
+		public void onClick(DialogInterface dialog, int whichButton) {
+		  Editable value = input.getText();
+			i.putExtra("workout_id", workout._id);
+			i.putExtra("workout_score",
+					Integer.parseInt(value.toString()));
+			startActivityForResult(i, ACT_TIMER);
+		  
+		  }
+		});
+
+		alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+		  public void onClick(DialogInterface dialog, int whichButton) {
+		    // Canceled.
+		  }
+		});
+
+		alert.show();
+		
+	}
+
 	protected void onActivityResult(int request, int result, Intent data)
 	{
 		if (request == ACT_TIMER) {
