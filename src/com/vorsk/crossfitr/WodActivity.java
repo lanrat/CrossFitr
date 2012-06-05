48d29c7863915de463a2d0f7cfdcf6749df183c2
NeoValkyrion <dan@bestangle.com>
2012-06-04 22:58:55 -0700

    Implemented UI for WOD workouts list

--- src/com/vorsk/crossfitr/WodActivity.java
+++ src/com/vorsk/crossfitr/WodActivity.java
@@ -2,12 +2,14 @@ package com.vorsk.crossfitr;
 
 import java.util.ArrayList;
 
+import com.vorsk.crossfitr.GirlsActivity.GirlsListHelper;
 import com.vorsk.crossfitr.models.WODModel;
 import com.vorsk.crossfitr.models.WorkoutModel;
 import com.vorsk.crossfitr.models.WorkoutRow;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.SQLException;
@@ -15,9 +17,14 @@ import android.graphics.Typeface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.View.OnClickListener;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -26,7 +33,8 @@ public class WodActivity extends Activity  implements OnItemClickListener
 {
 	private ListView wodView;
 	protected ProgressDialog pd;
-	ArrayAdapter<WorkoutRow> adapter;
+	ArrayAdapter<WorkoutRow> workoutrowList;
+	private WodListHelper listAdapter;
 	private static String TAG = "WODActivity";
 	
 	private TextView titleTextHeader1;
@@ -96,17 +104,21 @@ public class WodActivity extends Activity  implements OnItemClickListener
 
 	     protected void onPostExecute(ArrayList<WorkoutRow> results) {
 			
-			adapter = new ArrayAdapter<WorkoutRow>(context,
+			workoutrowList = new ArrayAdapter<WorkoutRow>(context,
 					android.R.layout.simple_list_item_1, android.R.id.text1,results);
 			stopLoadingScreen();
-	 		wodView.setAdapter(adapter);
+			
+			listAdapter = new WodListHelper(getApplicationContext(), results);
+			listAdapter.notifyDataSetChanged();
+			
+	 		wodView.setAdapter(listAdapter);
 
 	     }
 	 }
 
 	public void onItemClick(AdapterView<?> arg0, View view, int position, long id) {
 		//pass the ID of the workout into the WorkoutProfileActivity
-		WorkoutRow workout = adapter.getItem(position);
+		WorkoutRow workout = workoutrowList.getItem(position);
 		
 		//add the selected workout to the DB
 		WorkoutModel model = new WorkoutModel(this);
@@ -141,5 +153,75 @@ public class WodActivity extends Activity  implements OnItemClickListener
 		x.putExtra("ID", entry_id); 
 		startActivity(x);
 	}
+	
+	public class WodListHelper extends BaseAdapter implements OnClickListener {
+
+		private static final String tag = "WodListHelper";
+		private final Context listContext;
+		private ArrayList<WorkoutRow> arrayList;
+		private ImageView listArrow;
+		private TextView nameTView;
+		private TextView descTView;
+		private LayoutInflater inflater;
+
+		public WodListHelper(Context _context, ArrayList<WorkoutRow> _data) {
+			this.listContext = _context;
+			this.arrayList = _data;
+			inflater = (LayoutInflater) _context
+					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		}
+
+		public View getView(int index, View convertView, ViewGroup parent) {
+			
+			if (convertView == null)
+				convertView = inflater
+						.inflate(R.layout.custom_list_item, parent, false);
+
+			listArrow = (ImageView) convertView.findViewById(R.id.image_arrow);
+			listArrow.setOnClickListener(this);
+
+			Log.d(tag, "arrayList.get(" + index + ").name : "
+					+ arrayList.get(index).name);
+			
+			// workout name
+			nameTView = (TextView) convertView
+					.findViewById(R.id.string_nameofworkout);
+			nameTView.setText(arrayList.get(index).name);
+			nameTView.setTextColor(getResources().getColor(R.color.wod));	
+			nameTView.setTypeface(font);		
+			
+			//workout description
+			descTView = (TextView) convertView
+					.findViewById(R.id.string_description);
+			descTView.setText(arrayList.get(index).description);
+			descTView.setTextColor(getResources().getColor(R.color.white));
+			descTView.setSelected(true);
+			descTView.setTypeface(font);
+
+			return convertView;
+		}
+
+		public int getCount() {
+			// TODO Auto-generated method stub
+			return arrayList.size();
+		}
+
+		public String getItem(int index) {
+			return arrayList.get(index).name;
+		}
+
+		public <T> T get(int arg0) {
+			return (T) arrayList.get(arg0);
+		}
+
+		public long getItemId(int id) {
+			return id;
+		}
+
+		public void onClick(View arg0) {
+
+		}
+
+	}	
 
 }
